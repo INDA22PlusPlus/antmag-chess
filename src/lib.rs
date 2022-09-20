6c965f7b0e30ef71578fe8c99dc345e75bf2d6f8
antmag <antmag@kth.se>
2022-09-20 22:16:03 +0200

    Move generation

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,14 @@ board structure: Use array, [[]]
 enum to represent an arbitrary piece or empty; 
 Utilize a simple threat_buffer that is recomputed every move
 
+todo: Move generation
+    - Representing moves
+todo: Scheck mate
+
+
+Implament a generate_moves_simple for each piece type
+implament a move_check function that determines if the move is valid(simulate move and recompute threat buffer)
+
 */
 
 #[allow(unused_parens)]
@@ -15,6 +23,11 @@ Utilize a simple threat_buffer that is recomputed every move
 pub mod chess_api{
     use std::slice;
     
+    pub const WHITE : bool = false;
+    pub const BLACK : bool = true;
+    pub const MAX_MOVES : usize = 32;
+
+    #[derive(Clone, Copy)]
     pub struct Board_state{
         //state defining parameeters
         pub _board_types : [[Cell; 8]; 8],
@@ -27,7 +40,6 @@ pub mod chess_api{
         castling: [bool; 2],
         
         pub king_pos : [(usize, usize); 2],
-
     }
 
     #[derive(Clone, Copy)]
@@ -41,8 +53,25 @@ pub mod chess_api{
         None
     }
 
+    #[derive(Clone, Copy)]
+    pub enum Move_type{
+        Peassant,
+        Castling,
+        Capture,
+        Promotion,
+        Move
+    }
+
+    #[derive(Clone, Copy)]
+    pub struct Move{
+        pub from : (i32, i32),
+        pub to : (i32, i32),
+        pub typ : Move_type,
+        pub color : bool,
+    }
+
     pub mod Board{
-        use std::mem::swap;
+        use std::{mem::swap, option};
         use super::*;
 
         //Essentially a board constructor
@@ -84,6 +113,52 @@ pub mod chess_api{
             };
 
             return B;
+        }
+
+        /*Moves can be created if the following conditions are met: 
+        (1) there is a piece on from 
+        (2) If there is a piece at to, it is not of the same color
+        */
+        pub fn get_move_type(from : (i32, i32), to : (i32,i32), board : &Board_state) -> Option<Move_type>{
+            let to_type = board._board_types[to.0 as usize][to.1 as usize];
+            let from_type = board._board_types[from.0 as usize][from.1 as usize];
+            if let Cell::None = from_type {return None;}
+
+            let from_color = board._board_color[from.0 as usize][from.1 as usize]; //The color making a move
+            if let Cell::None = to_type{ //Promotion or Move
+                if let Cell::Pawn = from_type{
+                    if((to.0 == 0 && from_color == WHITE) || (to.0 == 7 && from_color == BLACK)){
+                        return Some(Move_type::Promotion);
+                    }else{
+                        return Some(Move_type::Move);
+                    }
+                }else{
+                    return Some(Move_type::Move);
+                }
+            }else{
+                if board._board_color[to.0 as usize][to.1 as usize] == from_color{
+                    //Check for castling here
+                    return None;
+                }else{ //Capture
+                    return Some(Move_type::Capture);
+                }
+            }; 
+        }
+
+        pub fn create_move(from : (i32, i32), to : (i32,i32), board : &Board_state) -> Option<Move> {
+            let typ = get_move_type(from, to, board);
+            
+            match typ{
+                Some(p) => {
+                    return Some(Move{
+                        from : from,
+                        to : to,
+                        typ : p,
+                        color : board._board_color[from.0 as usize][from.1 as usize]
+                    });
+                },
+                None => {return None;}
+            };
         }
 
         fn create_start_config(board : &mut Board_state, inverted : bool){
@@ -144,7 +219,23 @@ pub mod chess_api{
     
     pub mod util{
         use std::i32;
-        use super::*;
+        use super::{*, Board::create_move};
+
+        pub fn move_check(board : &Board_state, mv : &Move) -> bool{
+
+            //If we are moving a king
+            if let Cell::King = board._board_types[mv.from.0 as usize][mv.from.1 as usize] {
+                //Check if the color not doing the move is threatening the cell that we want to move the king to
+                if(board.threat_buff[(!mv.color) as usize][mv.to.0 as usize][mv.to.1 as usize] == true) {return false;}
+            }
+           
+            //We should not be able to make a move that will capture a king
+            if let Cell::King = board._board_types[mv.to.0 as usize][mv.to.1 as usize]{return false;}
+
+            //Make a copy of the board and simulate a move, update_threat_buffer, check if check.
+
+            return true;
+        }
 
         pub fn generate_threat_static(li : &[(i32, i32)], board : &mut Board_state, color : bool){
             for (row, col) in li.iter() {
@@ -170,11 +261,67 @@ pub mod chess_api{
                 col += dir.1;
             }
         }
+
+        pub fn construct_move_buffer() -> [Move; MAX_MOVES]{
+            return [Move {
+                from : (0,0),
+                to : (0,0),
+                typ : Move_type::Move,
+                color : false,
+            }; MAX_MOVES];
+        }
+
+        //We generate the moves for a piece of color(color) given the positions listed in li
+        pub fn generate_moves_simple_static(from : (i32,i32), li : &[(i32, i32)], board : &mut Board_state, move_buffer : &mut [Move; MAX_MOVES]) -> usize{
+            let mut buffer_idx: usize= 0;
+            assert!(buffer_idx < MAX_MOVES);
+
+            for (row, col) in li{
+                if(*row < 0 || *row > 7 || *col < 0 || *col > 7){ continue;}
+
+                //If this errors then the move we tried to create was not remotely valid
+                let res = create_move(from, (*row, *col), board);
+                if let Option::Some(p) = res {
+                    move_buffer[buffer_idx] = p;
+                    buffer_idx += 1;
+                }
+            }
+
+            return buffer_idx;
+        }
+
+        pub fn generate_moves_simple_dir(from : (i32,i32), dir : (i32, i32), board : &mut Board_state, move_buffer : &mut [Move; MAX_MOVES]) -> usize{
+            let mut buffer_idx: usize= 0;
+            assert!(buffer_idx < MAX_MOVES);
+
+            let from_color = board._board_color[from.0 as usize][from.1 as usize];
+            let mut to = (from.0 + dir.0, from.1 + dir.1);
+            let mut break_flag : bool = false;
+            
+            while(to.0 >= 0 && to.0 < 8 && to.1 >= 0 && to.1 < 8 && !break_flag){
+                let at = board._board_types[to.0 as usize][to.1 as usize];
+                let at_color = board._board_color[to.0 as usize][to.1 as usize];
+                if let Cell::None = at {} else{
+                    break_flag = true;
+                    if(from_color == at_color) {continue;}
+                }
+
+                move_buffer[buffer_idx] = create_move(from, to, board).unwrap(); //This is expected to work
+                buffer_idx += 1;
+                to.0 += dir.0;
+                to.1 += dir.1;
+            }
+
+            return buffer_idx;
+        }
+
+
     }
 
     pub mod Knight{
-        use super::Board_state;
-        use super::util::generate_threat_static;
+        use super::{Board_state, MAX_MOVES};
+        use super::Move;
+        use super::util::*;
 
         pub fn generate_threat(row : i32, col : i32, board : &mut Board_state, color : bool){
             let li : [(i32, i32); 8] = [
@@ -192,6 +339,24 @@ pub mod chess_api{
             ];
             generate_threat_static(&li, board, color);
         }
+
+        pub fn generate_moves_simple(row : i32, col : i32, board: &mut Board_state, move_buffer : &mut [Move; MAX_MOVES]) -> usize{
+            let li : [(i32, i32); 8] = [
+                (row+1, col-2),
+                (row+2, col-1),
+    
+                (row+2, col+1),
+                (row+1, col+2),
+                
+                (row-1, col+2),
+                (row-2, col+1),
+                
+                (row-2, col-1),
+                (row-1, col-2)
+            ];
+            return generate_moves_simple_static((row, col), &li, board, move_buffer);
+        }
+
         /* 
         pub fn generate() -> u64{
             
@@ -200,8 +365,10 @@ pub mod chess_api{
     }
 
     pub mod Bishop{
+        use super::MAX_MOVES;
         use super::Board_state;
-        use super::util::generate_threat_dir;
+        use super::Move;
+        use super::util::*;
 
         pub fn generate_threat(row : i32, col : i32, board : &mut Board_state, color : bool){
             let dir_li : [(i32,i32); 4] = [
@@ -214,11 +381,29 @@ pub mod chess_api{
                 generate_threat_dir(row, col, dir, board, color);
             }
         }
+
+        pub fn generate_moves_simple(row : i32, col : i32, board: &mut Board_state, move_buffer : &mut [Move; MAX_MOVES]) -> usize{
+            let dir_li : [(i32,i32); 4] = [
+                (-1,-1),
+                (-1,1),
+                (1,-1),
+                (1,1)
+            ];
+            let mut curr_cnt = 0;
+
+            for dir in dir_li{
+                curr_cnt += generate_moves_simple_dir((row,col), dir, board, move_buffer);
+            }
+            return curr_cnt;
+        }
+
     }
 
     pub mod Rook{
         use super::Board_state;
-        use super::util::generate_threat_dir;
+        use super::util::*;
+        use super::Move;
+        use super::MAX_MOVES;
 
         pub fn generate_threat(row : i32, col : i32, board : &mut Board_state, color : bool){
             let dir_li : [(i32, i32); 4] = [
@@ -232,11 +417,28 @@ pub mod chess_api{
                 generate_threat_dir(row, col, dir, board, color);
             }
         }
+
+        pub fn generate_moves_simple(row : i32, col : i32, board: &mut Board_state, move_buffer : &mut [Move; MAX_MOVES]) -> usize{
+            let dir_li : [(i32, i32); 4] = [
+                (1,0),
+                (-1,0),
+                (0,1),
+                (0,-1)
+            ];
+            let mut curr_cnt = 0;
+            for dir in dir_li{
+                curr_cnt += generate_moves_simple_dir((row, col), dir, board, move_buffer);
+            }
+            return curr_cnt;
+        }
+
     }
 
     pub mod Queen{
         use super::Board_state;
-        use super::util::generate_threat_dir;
+        use super::util::*;
+        use super::MAX_MOVES;
+        use super::Move;
 
         pub fn generate_threat(row : i32, col : i32, board : &mut Board_state, color : bool){
             let dir_li : [(i32, i32); 8] = [
@@ -254,11 +456,33 @@ pub mod chess_api{
                 generate_threat_dir(row, col, dir, board, color);
             }
         }
+
+        pub fn generate_moves_simple(row : i32, col : i32, board: &mut Board_state, move_buffer : &mut [Move; MAX_MOVES]) -> usize{
+            let dir_li : [(i32, i32); 8] = [
+                (1, 0),
+                (-1,0),
+                (0,1),
+                (0,-1),
+                (-1, -1),
+                (-1,1),
+                (1,-1),
+                (1,1)
+            ];
+            
+            let mut curr_cnt = 0;
+            for dir in dir_li{
+                curr_cnt += generate_moves_simple_dir((row, col), dir, board, move_buffer);
+            }
+            return curr_cnt;
+        }
+
     }
 
     pub mod King{
         use super::Board_state;
-        use super::util::generate_threat_static;
+        use super::util::*;
+        use super::MAX_MOVES;
+        use super::Move;
 
         pub fn generate_threat(row : i32, col : i32, board : &mut Board_state, color : bool){
             let li : [(i32, i32); 8] = [
@@ -275,21 +499,30 @@ pub mod chess_api{
 
             generate_threat_static(&li, board, color);
         }
+
+        pub fn generate_moves_simple(row : i32, col : i32, board: &mut Board_state, move_buffer : &mut [Move; MAX_MOVES]) -> usize{
+            let li : [(i32, i32); 8] = [
+                (row+1, col+1),
+                (row-1, col+1),
+                (row+1, col-1),
+                (row-1, col-1),
+                
+                (row-1, col),
+                (row, col-1),
+                (row+1, col),
+                (row, col+1)
+            ];
+
+            return generate_moves_simple_static((row,col), &li, board, move_buffer);
+        }
     }
 
     pub mod Pawn{
-        use super::Board_state;
-        use super::util::generate_threat_static;
+        use super::util::*;
+        use super::*;
         
         pub fn generate_threat(row : i32, col : i32, board : &mut Board_state, color : bool){
-            let mut row_increment = if color == false {-1} else {1}; //if color is white
-            if(board.inverted == true) { //The board could be inverted
-                if(row_increment == 1) { // asuming row increment is either 1 or -1
-                    row_increment = -1;
-                }else{
-                    row_increment = 1;
-                }
-            } 
+            let row_increment = if color == false {-1} else {1}; //if color is white
             
             let li : [(i32,i32); 2] = [
                 (row + row_increment, col + 1),
@@ -298,11 +531,24 @@ pub mod chess_api{
             generate_threat_static(&li, board, color);
         }
 
+        pub fn generate_moves_simple(row : i32, col : i32, color : bool, board: &mut Board_state, move_buffer : &mut [Move; MAX_MOVES]) -> usize{
+            let row_increment = if color == false {-1} else {1}; //if color is white
+            let mut li : [(i32,i32); 3] = [
+                (row + row_increment, col),
+                (-1, -1), //Terrible workaround, these will be ignored by the static generator
+                (-1, -1)
+            ];
+            
+            if let Cell::None = board._board_types[(row + row_increment) as usize][(col-1) as usize] {} else {
+                li[1] = (row + row_increment, col-1);
+            }
+            if let Cell::None = board._board_types[(row + row_increment) as usize][(col+1) as usize] {} else {
+                li[2] = (row + row_increment, col+1);
+            }
+            return generate_moves_simple_static((row, col), &li, board, move_buffer);
+        }
+
     }
-
-    
-
-    
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,14 @@ fn main() {
 
 #[cfg(test)]
 mod test{
+
     use test_crate::chess_api::*;
     use test_crate::chess_api::Board::*;
+    use test_crate::chess_api::util::*;
+    use test_crate::chess_api::Queen::*;
+
+    use crate::test;
+
 
     #[test]
     fn test_init_config(){
@@ -78,6 +84,90 @@ mod test{
         assert_eq!(scheck(&b, true), true);
     }
 
+    #[test]
+    fn move_test_1(){
+        let mut b : Board_state = create_blank_board(false);
+        b._board_types[1][5] = Cell::Pawn;
+        b._board_color[0][5] = false;
+        let li = [
+            (0,5),
+        ];
+
+        let mut move_buffer:[Move; MAX_MOVES] = construct_move_buffer();
+
+        let cnt = generate_moves_simple_static((1,5), &li, &mut b, &mut move_buffer);
+        assert_eq!(cnt, 1);
+        if let Move_type::Promotion = move_buffer[0].typ {} else {panic!()}
+    }
+
+    #[test]
+    fn move_test_2(){
+        let mut b : Board_state = create_blank_board(false);
+        b._board_types[3][5] = Cell::King;
+        b._board_color[0][5] = true;
+
+        let li = [
+            (2,5),
+            (3,4),
+            (3,6)
+        ];
+        let mut move_buffer:[Move; MAX_MOVES] = construct_move_buffer();
+        let cnt = generate_moves_simple_static((3,5), &li, &mut b, &mut move_buffer);
+        assert_eq!(cnt, 3);
+        
+        for i in 0..3 {
+            if let Move_type::Move = move_buffer[i].typ {} else {panic!();}
+        }
+    }
+
+    #[test]
+    fn move_test_3(){
+        let mut b : Board_state = create_blank_board(false);
+        b._board_types[3][5] = Cell::Knight;
+        b._board_types[1][4] = Cell::Pawn;
+        b._board_color[3][5] = false;
+        b._board_color[1][4] = true;
+        let mut move_buffer = construct_move_buffer();
+        
+        let cnt = generate_moves_simple_static((3,5), &[(1,4)], &mut b, &mut move_buffer);
+        assert_eq!(cnt, 1);
+        if let Move_type::Capture = move_buffer[0].typ {} else {panic!();}
+    }
+
+    #[test]
+    fn actual_move_test_1(){
+        let mut b : Board_state = create_blank_board(false);
+        b._board_types[4][4] = Cell::Queen;
+        b._board_types[3][4] = Cell::Pawn;
+        b._board_types[4][1] = Cell::Pawn;
+
+        b._board_color[4][4] = BLACK;
+        b._board_color[3][4] = BLACK;
+        b._board_color[4][1] = WHITE;
+
+        let mut move_buffer = construct_move_buffer();
+
+        let cnt = Queen::generate_moves_simple(4, 4, &mut b, &mut move_buffer);
+        assert_eq!(cnt, 22);
+    }
+
+    #[test]
+    fn actual_move_test_2(){
+        let mut b : Board_state = create_blank_board(false);
+        b._board_types[0][7] = Cell::King;
+        b._board_types[3][4] = Cell::Knight;
+        b._board_color[0][7] = BLACK;
+        b._board_color[3][4] = WHITE;
+
+        let mut move_buffer_1 = construct_move_buffer();
+        let cnt_1 = King::generate_moves_simple(0, 7, &mut b, &mut move_buffer_1);
+
+        let mut move_buffer_2 = construct_move_buffer();
+        let cnt_2 = Knight::generate_moves_simple(3, 4, &mut b, &mut move_buffer_2);
+
+        assert_eq!(cnt_1, 3);
+        assert_eq!(cnt_2, 8);
+    }
 
 
 }
